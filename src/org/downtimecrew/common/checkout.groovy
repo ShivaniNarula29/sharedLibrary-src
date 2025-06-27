bebcf898b2ecf758b7f0a5444014f762233d4bb4
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-27 18:52:36 +0530

    Create checkout.groovy

---
+++ src/org/downtimecrew/common/checkout.groovy
@@ -0,0 +1,14 @@
+package org.downtimecrew.common
+
+def call(Map config = [:]) {
+    stage(config.stageName ?: 'Checkout Code') {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: config.branch ?: env.GIT_BRANCH ?: 'main']],
+                userRemoteConfigs: [[
+                    url: config.repoUrl ?: env.GIT_REPO_URL ?: 'https://github.com/OT-MICROSERVICES/frontend.git',
+                    credentialsId: config.creds ?: env.GIT_CREDS_ID ?: 'default-credentials-id'
+                ]]
+            ])
+        }
+}
